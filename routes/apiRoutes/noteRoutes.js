bfc8c4ae5cbbaf9be3dd44d18c16ee3664c3bfb5
cam-cole <camcole065@gmail.com>
2022-06-20 16:22:35 -0500

    continuing work on routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const { notes } = require('../../db/db.json');
+const { notes } = require('../../db/db');
 
 router.get('/notes', (req,res) => {
-    let results = notes;
-    res.json(results);
+    response = notes;
+    res.json(response);
 });
 
 module.exports = router;
