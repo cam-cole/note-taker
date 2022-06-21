c45b762889b00400869223cefad44ac31bbe56ab
cam-cole <camcole065@gmail.com>
2022-06-20 19:48:22 -0500

    finished routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db');
+const createNewNote = require('../../lib/notes');
 
 router.get('/notes', (req,res) => {
     response = notes;
     res.json(response);
 });
+
+router.post('/notes', (req,res) => {
+    req.body.id = notes.length.toString();
+
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+})
 
 module.exports = router;
